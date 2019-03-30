60ba4a0b14fa954cf16be0ccd1cc74756ed18c15
PINAKSH KUMAR <36802422+wireshark10@users.noreply.github.com>
2019-03-30 07:50:30 +0530

    Added
    task2(a).py

---
+++ task2(a).py
@@ -0,0 +1,29 @@
+#In python split the pdf file into individual pages
+
+
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+
+def pdf_splitter(path):
+    fname = os.path.splitext(os.path.basename(path))[0]
+
+    pdf = PdfFileReader(path)
+    for page in range(pdf.getNumPages()):
+        pdf_writer = PdfFileWriter()
+        pdf_writer.addPage(pdf.getPage(page))
+
+        output_filename = '{}_page_{}.pdf'.format(
+            fname, page+1)
+
+        with open(output_filename, 'wb') as out:
+            pdf_writer.write(out)
+
+
+
+if __name__ == '__main__':
+    path = '/home/wireshark10/GSOC/CERN.pdf'
+
+
+
+    pdf_splitter(path)

7f3de19e1a03f1489d78263eb575139bdb529177
PINAKSH KUMAR <36802422+wireshark10@users.noreply.github.com>
2019-04-04 06:31:59 +0530

    Add files via upload

---
+++ ganga ArgSplitter job task2(c)/input/task2(c).py
@@ -0,0 +1,20 @@
+#THIS SCRIPT IS CONVERTED TO AN EXECUTABLE THAT GANGA WILL TAKE AS AN APPLICATION .CONVERTED TO AN EXECUTABLE USING Pyinstaller.
+
+
+import os
+import re
+import sys
+wordcount={}
+filename=sys.argv[1]
+f = open(filename) 
+for word in f.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+
+f.close()
+word="the"
+
+print word, wordcount[word]
+wordcount[word]=0
